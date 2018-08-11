9c32048a369d1fe35077d71aeb01790879809ecf
Loïck Virot <virot.loick@gmail.com>
2018-08-11 16:27:05 +0200

    Create battery graph

--- front/src/components/pages/bike/BikeView.vue
+++ front/src/components/pages/bike/BikeView.vue
@@ -5,9 +5,14 @@
       <div class="column column-left">
         <card-box>
           <card-content>
-            <h2>Statistiques</h2>
             <div class="battery">
-              
+              <div class="battery-chart">
+                <canvas id="batteryChart" width="150" height="150"></canvas>
+              </div>
+              <div class="battery-percentage">
+                <span>BATTERIE</span>
+                <h1>70%</h1>
+              </div>
             </div>
           </card-content>
         </card-box>
@@ -46,6 +51,34 @@ export default {
       ],
     };
   },
+  mounted() {
+    let ctx = document.getElementById("batteryChart").getContext('2d');
+    let myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Batterie restante", "Batterie finie"],
+        datasets: [{
+          data: [70, 100-70],
+          backgroundColor: [
+            '#40b883',
+            '#EAEAEA'
+          ],
+        }],
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          enabled: false,
+        },
+        rotation: -0.5 * Math.PI,
+        hover: {mode: null},
+        maintainAspectRatio: false,
+        cutoutPercentage: 70,
+      }
+    });
+  },
 };
 </script>
 <style>
@@ -62,5 +95,28 @@ export default {
   .column {
     margin: 10px;
     flex: 1;
+  }
+
+  .battery {
+    display: flex;
+  }
+
+  .battery-chart {
+    width: 150px;
+  }
+
+  .battery-percentage {
+    margin: auto 20px;
+  }
+
+  .battery-percentage span {
+    font-weight: bold;
+    color: #CCC;
+  }
+
+  .battery-percentage h1 {
+    font-weight: 100;
+    font-size: 3.5em;
+    margin: 0;
   }
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,25 +1,25 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMap, faCog, faMotorcycle, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
-import VueRouter from 'vue-router'
-import store from './store'
-import routes from './routes'
+import store from './store';
+import routes from './routes';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMap, faCog, faMotorcycle, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-// Router 
-Vue.use(VueRouter)
+// Router
+Vue.use(VueRouter);
 const router = new VueRouter({
-  routes
+  routes,
 });
 
-library.add(faMap)
-library.add(faCog)
-library.add(faMotorcycle)
-library.add(faArrowLeft)
+library.add(faMap);
+library.add(faCog);
+library.add(faMotorcycle);
+library.add(faArrowLeft);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
