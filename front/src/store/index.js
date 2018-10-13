c15d0867b1be7f1c77663ca4acae7f863e72674b
LoickVirot <virot.loick@gmail.com>
2018-10-13 18:27:19 +0200

    persist state in localStorage

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import bike from './modules/bike';
 import auth from './modules/auth';
+import VuexPersist from 'vuex-persist'
+
+
+const vuexLocal = new VuexPersist({
+  key: 'vuex',
+  storage: localStorage
+})
 
 Vue.use(Vuex);
 
@@ -11,4 +18,5 @@ export default new Vuex.Store({
     auth: auth,
   },
   strict: false,
+  plugins: [vuexLocal.plugin]
 });
